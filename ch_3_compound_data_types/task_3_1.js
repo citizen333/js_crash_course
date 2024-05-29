a21c530bb41d6ed74aab756383e8e736fe456cd8
citizen333 <a.t.kalinin@gmail.com>
2024-05-30 01:20:50 +0300

    wrote tic-tac-toe in imperative style

--- ch_3_compound_data_types/task_3_1.js
+++ ch_3_compound_data_types/task_3_1.js
@@ -1,4 +1,6 @@
 const prompt = require('prompt-sync')();
+player = Math.round(Math.random())
+resume = 1
 
 ticTacToeArray = []
 arraySize = Number(prompt("Введите размер поля: "))
@@ -6,9 +8,64 @@ arraySize = Number(prompt("Введите размер поля: "))
 for (let i = 0; i < arraySize; i++) {
     ticTacToeRow = []
     for (let j = 0; j < arraySize; j++) {
-        ticTacToeRow.push('')
+        ticTacToeRow.push(-9)
     }
     ticTacToeArray.push(ticTacToeRow)
 }
 
 console.log(ticTacToeArray)
+
+while (true) {
+    while (true) {
+        [row, col] = prompt(`Ход игрока ${player}: `)
+        if (ticTacToeArray[Number(row)][Number(col)] < 0) {
+            break
+        }
+        console.log("Эта ячейка занята, введите другую")
+    }
+    ticTacToeArray[Number(row)][Number(col)] = player 
+    
+    console.log(ticTacToeArray)
+
+    checkArray = []
+    for (let row of ticTacToeArray) {
+        checkArray.push(row.slice())
+    }
+
+    for (let i in ticTacToeArray) {
+        checkArray.push([])
+    }
+    
+    for (let i = 0; i < ticTacToeArray.length; i++) {
+        for (let j = 0; j < ticTacToeArray.length; j++) {
+            checkArray[j + ticTacToeArray.length].push(ticTacToeArray[i][j])
+        }
+    }
+    checkArray.push([])
+    checkArray.push([])
+    for (let i = 0; i < ticTacToeArray.length; i++) {
+        checkArray[checkArray.length - 2].push(ticTacToeArray[i][i])
+        checkArray[checkArray.length - 1].push(ticTacToeArray[i][ticTacToeArray.length - 1 - i])
+    }
+    
+    console.log(checkArray)
+    
+    for (row of checkArray) {
+        rowSum = 0
+        for (elem of row) {
+            rowSum += elem
+        }
+        if (rowSum != 0 && rowSum != ticTacToeArray.length) {
+            continue
+        }
+        console.log(`Игрок ${player} выиграл`)
+        resume = 0
+        break
+    }
+    if (resume == 0) {
+        break
+    }
+    console.log("Ход переходит следующему игроку")
+    player = player == 1 ? 0 : 1
+}
+
