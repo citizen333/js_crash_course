2f5154190e73c4a341c58c8a9e3216dec9177350
citizen333 <a.t.kalinin@gmail.com>
2024-06-01 12:42:32 +0300

    added functions to finalize tic tac toe and created a game object to manipulate game. good enough

--- ch_3_compound_data_types/task_3_1.js
+++ ch_3_compound_data_types/task_3_1.js
@@ -1,71 +1,153 @@
 const prompt = require('prompt-sync')();
-player = Math.round(Math.random())
-resume = 1
 
-ticTacToeArray = []
-arraySize = Number(prompt("Введите размер поля: "))
-
-for (let i = 0; i < arraySize; i++) {
-    ticTacToeRow = []
-    for (let j = 0; j < arraySize; j++) {
-        ticTacToeRow.push(-9)
+function createGrid(gridDim, blankValue) {
+    grid = []
+    for (let i = 0; i < gridDim; i++) {
+        row = []
+        for (let j = 0; j < gridDim; j++) {
+            row.push(blankValue)
+        }
+        grid.push(row)
     }
-    ticTacToeArray.push(ticTacToeRow)
+    return grid
 }
 
-console.log(ticTacToeArray)
+function createExtendedGrid(baseGrid) {
+    newGrid = []
+    for (let row of baseGrid) {
+        newGrid.push(row.slice())
+    }
 
-while (true) {
+    for (let i = 0; i < baseGrid.length; i++) {
+        newGrid.push([])
+    }
+    for (let i = 0; i < baseGrid.length; i++) {
+        for (let j = 0; j < baseGrid.length; j++) {
+            newGrid[j + baseGrid.length].push(baseGrid[i][j])
+        }
+    }
+
+    newGrid.push([])
+    newGrid.push([])
+    for (let i = 0; i < baseGrid.length; i++) {
+        newGrid[newGrid.length - 2].push(baseGrid[i][i])
+        newGrid[newGrid.length - 1].push(baseGrid[i][baseGrid.length - 1 - i])
+    }
+
+    return newGrid
+}
+
+function getWinner(grid) {
+    let checkGrid = createExtendedGrid(grid)
+    for (row of checkGrid) {
+
+        let rowSum = 0
+        for (elem of row) {
+            rowSum += elem
+        }
+
+        if (Math.abs(rowSum) < 3) {
+            continue
+        }
+
+        let winner = row[0]
+        return winner
+    }
+    return null
+}
+
+function checkFull(grid) {
+    let gridProduct = 1
+    for (row of grid) {
+        for (elem of row) {
+            gridProduct *= elem
+        }
+    }
+    return gridProduct != 0
+}
+
+function displayGrid(grid, gridMap) {
+    console.log()
+    for (let i = 0; i < grid.length; i++) {
+        gridRow = ''
+        for (let j = 0; j < grid[i].length; j++) {
+            gridRow += gridMap[grid[i][j]]
+            if (j != grid[i].length - 1) {
+                gridRow += ' '
+            }
+        }
+        console.log(gridRow)
+    }
+    console.log()
+}
+
+function makeMove(playerName, playerSign = playerName, grid) {
     while (true) {
-        [row, col] = prompt(`Ход игрока ${player}: `)
-        if (ticTacToeArray[Number(row)][Number(col)] < 0) {
+        [row, col] = prompt(`Ход игрока ${playerName}: `)
+        if (grid[Number(row)][Number(col)] == 0) {
             break
         }
         console.log("Эта ячейка занята, введите другую")
     }
-    ticTacToeArray[Number(row)][Number(col)] = player 
-    
-    console.log(ticTacToeArray)
-
-    checkArray = []
-    for (let row of ticTacToeArray) {
-        checkArray.push(row.slice())
-    }
-
-    for (let i in ticTacToeArray) {
-        checkArray.push([])
-    }
-    
-    for (let i = 0; i < ticTacToeArray.length; i++) {
-        for (let j = 0; j < ticTacToeArray.length; j++) {
-            checkArray[j + ticTacToeArray.length].push(ticTacToeArray[i][j])
-        }
-    }
-    checkArray.push([])
-    checkArray.push([])
-    for (let i = 0; i < ticTacToeArray.length; i++) {
-        checkArray[checkArray.length - 2].push(ticTacToeArray[i][i])
-        checkArray[checkArray.length - 1].push(ticTacToeArray[i][ticTacToeArray.length - 1 - i])
-    }
-    
-    console.log(checkArray)
-    
-    for (row of checkArray) {
-        rowSum = 0
-        for (elem of row) {
-            rowSum += elem
-        }
-        if (rowSum != 0 && rowSum != ticTacToeArray.length) {
-            continue
-        }
-        console.log(`Игрок ${player} выиграл`)
-        resume = 0
-        break
-    }
-    if (resume == 0) {
-        break
-    }
-    console.log("Ход переходит следующему игроку")
-    player = player == 1 ? 0 : 1
+    grid[Number(row)][Number(col)] = playerSign 
+    return grid
 }
 
+function initGame() {
+    const gameMap = new Object()
+    gameMap.player = Math.round(Math.random()) * 2 - 1
+    gameMap.getWinner = function() {
+        return getWinner(gameMap.grid.self)
+    }
+    
+    const grid = new Object()
+    grid.map = {
+        "-1": "o",
+        1: "x",
+        0: "_"
+    }
+    grid.dim = Number(prompt("Введите размер поля: "))
+    grid.self = createGrid(grid.dim, 0)
+    grid.display = function() {
+        return displayGrid(gameMap.grid.self, gameMap.grid.map)
+    }
+    gameMap.grid = grid
+
+    return gameMap
+}
+
+let game = initGame()
+game.grid.display()
+
+while (true) {
+    makeMove(
+        game.grid.map[game.player],
+        game.player,
+        game.grid.self
+    )
+    game.grid.display()
+    game.getWinner()
+    
+    if (game.getWinner() == null && !checkFull(game.grid.self)) {
+        game.player = game.player == 1 ? -1 : 1
+        console.log(`Ход переходит игроку ${game.grid.map[game.player]}`)
+        console.log()
+        continue
+    }
+
+    if (game.getWinner() != null) {
+        console.log(`Игрок ${game.grid.map[game.player]} выиграл`)
+    } else {
+        console.log('В этот раз без победителя')
+    }
+    
+    newGame = prompt("Сыграть ещё раз (y/n)? ")
+    console.log()
+    if (newGame == 'y') {
+        game = initGame()
+        game.grid.display()
+        continue
+    }
+
+    break
+}
